0148e501df199c5e68bb710d55178a314afb9846
RiadhT <riadh_t@iol7-183.localdomain>
2019-11-16 20:20:25 +0100

    react-firebase-authentication

---
+++ src/components/Landing/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        );
+    }
+}
+
+export default index;
---
+++ src/components/Session/index.js
@@ -0,0 +1,8 @@
+import AuthUserContext from './context'
+import withAuthentication from './withAuthentication'
+import withAuthorization from './withAuthorization'
+
+
+export {AuthUserContext,
+    withAuthorization,
+    withAuthentication }
